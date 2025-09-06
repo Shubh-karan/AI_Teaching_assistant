223fd4a08e856f71fb7131bbf0937ae3cd2d3b0f
Shubh <shubhkaran255@gmail.com>
2025-09-06 17:19:01 +0530

    now added program in ATT(Audio to text) which converts audio to json file and saves those files in jsons folder

--- att.py
+++ att.py
@@ -1,6 +1,31 @@
 #just for info, importing, downloading and using model takes a lot of time, CPU, GPU and Memory, for safer side please go for Open AI API's
 import whisper
+import os
+import json
+
+os.makedirs("RAG_Project/jsons", exist_ok=True)
+
+audios = os.listdir("RAG_Project/Audio")
 
 model = whisper.load_model("large-v2")
-result = model.transcribe(audio="RAG_Project/Audio/2_Conditional Probability in 5 Minutes.mp3.mp3",word_timestamps=False)
-print(result)
+
+for audio in audios:
+    if("_" in audio):
+        number = audio.split("_")[0]
+        title = audio.split("_")[1][:-4]
+        result = model.transcribe(audio=f"RAG_Project/Audio/{audio}",
+                                  word_timestamps=False)
+
+        chunks = []
+        for segment in result['segments']:
+            chunks.append({"number" : number,
+                            "title" : title,
+                            "start" : segment["start"],
+                            "end" : segment["end"],
+                            "text" : segment["text"]})
+            
+        chunks_with_metadata = {"chunks" : chunks,"text" : result["text"]}
+
+        #this create a json file with the same name filename.mp3.json and save it in jsons folder from where we can analyze chunks
+        with open(f"RAG_Project/jsons/{audio}.json","w") as f:
+            json.dump(chunks_with_metadata,f,ensure_ascii=False,indent=2)
--- rename.py
+++ rename.py
@@ -7,7 +7,7 @@ filename = os.listdir(folder)
 for i,files in enumerate(filename,start=1):
     old_path = os.path.join(folder,files)
     fname = files.split(" ｜ ")[0]
-    new_name = f"{i}_{fname}.mp3"
+    new_name = f"{i}_{fname}"
     new_path = os.path.join(folder,new_name)
     os.rename(old_path,new_path)
     print(f'Renamed {fname} -> {new_name}')
